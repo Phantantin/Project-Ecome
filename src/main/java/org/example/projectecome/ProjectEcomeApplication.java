6f9af6feec82f373d4cca1db2ef160b0b4854a2a
Tan Tin <tantin10082003@gmail.com>
2025-02-11 09:41:03 +0700

    first

---
+++ src/main/java/org/example/projectecome/ProjectEcomeApplication.java
@@ -0,0 +1,13 @@
+package org.example.projectecome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEcomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectEcomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projectecome/ProjectEcomeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projectecome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEcomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
